548d2c2f900c68b95461803c9140e4d6148c52ff
Folashade <folajoachim@yahoo.co.uk>
2022-10-08 18:31:35 +0100

    Added links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+        <footer>
+          This project was coded by Folashade Joachim and is{" "}
+          <a
+            href="https://github.com/Folashade633/React-Weather-App"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://ornate-dragon-523f6d.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hosted on Netlify
+          </a>
+        </footer>
+      </div>
   );
 }
-
-export default App;
