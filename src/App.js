28a2146808b68ba55484a05563ff3e796b38af58
Folashade <folajoachim@yahoo.co.uk>
2022-10-08 18:55:47 +0100

    Added necessary files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from "react";
+
+        import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      
-        <footer>
-          This project was coded by Folashade Joachim and is{" "}
-          <a
-            href="https://github.com/Folashade633/React-Weather-App"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://ornate-dragon-523f6d.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hosted on Netlify
-          </a>
-        </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
       </div>
+    </div>
   );
 }
+  
